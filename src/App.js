ea154fb7f63c6244fc5be3c8590d923bd37180b3
erwanitwish <quillec.erwan@gmail.com>
2020-04-08 23:52:33 +0200

    good version

--- src/App.js
+++ src/App.js
@@ -6,31 +6,28 @@ import QuoteForm from './components/QuoteForm';
 import QuoteCardRandom from './components/QuoteCardRandom'
 import axios from 'axios'
 
-const sampleQuote = [
-  {
-    quote:
-      "Facts are meaningless. You could use facts to prove anything that's even remotely true.",
-    character: "Homer Simpson",
-    image:
-      "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939"
-  }]
-
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state= {
-      quote: 'https://simpsons-quotes-api.herokuapp.com/quotes'
+      quote: 'Shut up, brain. I got friends now. I don\'t need you anymore.',
+      character: "Lisa Simpson",
+      image: "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FLisaSimpson.png?1497567512083",
     }
   }
+
   getQuote = () => {
     axios.get('https://simpsons-quotes-api.herokuapp.com/quotes')
     .then(response => response.data)
     .then(data => {
       console.log(data);
       this.setState({
-        quote: data[0]});
-    })
+        quote: data[0].quote,
+        character: data[0].character,
+        image: data[0].image,
+        });
+    });
   }
 
   render() {
@@ -39,7 +36,7 @@ class App extends React.Component {
             <Navbar />
             <QuoteForm />
             <QuoteList />
-            <QuoteCardRandom quote={this.state.quote} />
+            <QuoteCardRandom quote={this.state} />
             <button type="button" onClick={this.getQuote}>Get Quote</button>
       </div>
     );
